45b509578174c06ef5701033a24535215698906d
Sonu7788 <sonujangir992002@gmail.com>
2025-12-13 09:25:58 +0530

    Added railway.json for ML deploy

--- server/index.js
+++ server/index.js
@@ -10,7 +10,12 @@ import analyticsRoutes from "./routes/analytics.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
